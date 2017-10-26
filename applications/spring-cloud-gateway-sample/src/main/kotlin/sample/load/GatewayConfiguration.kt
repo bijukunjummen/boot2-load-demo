979232fc12c4dfc27c00609a9855309e7443c973
Spencer Gibb <spencer@gibb.us>
2017-10-25 21:36:40 -0400

    Update to Finchley.BUILD-SNAPSHOT

--- applications/spring-cloud-gateway-sample/src/main/kotlin/sample/load/GatewayConfiguration.kt
+++ applications/spring-cloud-gateway-sample/src/main/kotlin/sample/load/GatewayConfiguration.kt
@@ -1,37 +1,26 @@
 package sample.load
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.cloud.gateway.filter.factory.RewritePathWebFilterFactory
-import org.springframework.cloud.gateway.filter.factory.RewritePathWebFilterFactory.REGEXP_KEY
-import org.springframework.cloud.gateway.filter.factory.RewritePathWebFilterFactory.REPLACEMENT_KEY
+import org.springframework.cloud.gateway.filter.factory.GatewayFilters.setPath
 import org.springframework.cloud.gateway.handler.predicate.RoutePredicates.path
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.Routes
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.tuple.TupleBuilder
-import org.springframework.web.server.WebFilter
 
 @Configuration
 class GatewayConfiguration {
     @Value("\${loadtarget.host}")
-    lateinit var targetHost: String;
+    lateinit var targetHost: String
 
     @Bean
     fun routes(): RouteLocator {
         return Routes.locator()
                 .route("passthrough")
+                .predicate(path("/passthrough/{segment}"))
+                .filter(setPath("/{segment}"))
                 .uri(targetHost)
-                .predicate(path("/passthrough/messages"))
-                .add(rewritePathFilter())
-                .and()
                 .build()
     }
 
-    @Bean
-    fun rewritePathFilter(): WebFilter {
-        return RewritePathWebFilterFactory()
-                .apply(TupleBuilder
-                        .tuple().of(REGEXP_KEY, "/passthrough/messages", REPLACEMENT_KEY, "/messages"))
-    }
 }
