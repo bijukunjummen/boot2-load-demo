fb1ebb1f262ef0d3182650a6af667b221ab814d8
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-11-19 20:38:00 -0800

    With micrometer/prometheus integration

--- applications/sample-load-target/src/main/kotlin/sample/load/MessageHandler.kt
+++ applications/sample-load-target/src/main/kotlin/sample/load/MessageHandler.kt
@@ -1,5 +1,6 @@
 package sample.load
 
+import io.micrometer.core.instrument.Metrics
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.BodyInserters.fromObject
@@ -11,8 +12,12 @@ import java.time.Duration
 
 @Service
 class MessageHandler {
+    
+    private val counter = Metrics.counter("handler.calls", "uri", "/messages")
+    
     fun handleMessage(req: ServerRequest): Mono<ServerResponse> {
         return req.bodyToMono<Message>().flatMap { m ->
+            counter.increment()
             Mono
                     .fromCallable({ MessageAck(id = m.id, received = m.payload, ack = "ack") })
                     .delayElement(Duration.ofMillis(m.delay))
