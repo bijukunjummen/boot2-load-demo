fc740f1acec4521b482ea172a16a3a7047f78d40
Biju Kunjummen <biju.kunjummen@gmail.com>
2018-04-04 16:00:36 -0700

    Additional micrometer tag for zuul apps

--- applications/zuul-sample/src/main/java/sample/zuul/ZuulSampleApplication.java
+++ applications/zuul-sample/src/main/java/sample/zuul/ZuulSampleApplication.java
@@ -1,8 +1,11 @@
 package sample.zuul;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,5 +13,12 @@ public class ZuulSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulSampleApplication.class, args);
+    }
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> commonTags() {
+        return registry ->
+                registry.config()
+                        .commonTags("application", "zuul-sample");
     }
 }
